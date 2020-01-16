68e7867a2e2ea67ef36430c71a199f839c8309e8
Md Prodhan <mdmarufprodhan@labs-mac.local>
2020-01-16 16:40:55 +0600

    Initial Commit

---
+++ API_callByCocoapod/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  API_callByCocoapod
+//
+//  Created by Md Maruf Prodhan on 1/16/20.
+//  Copyright © 2020 Md Maruf Prodhan. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+
+# platform :ios, '9.0'
+
+target 'API_callByCocoapod' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Alamofire', '4.4.0'
+  
+
+end
